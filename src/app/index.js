2b4a12b6ddd71140b9134efd17501a37bbd59646
Cardante <diogomotacardante@hotmail.com>
2018-07-05 20:03:41 +0100

    React First Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,25 @@
-console.log("Testing index.js");
+import React from 'react'; //for creating components
+import { render } from "react-dom"; //for component render
+
+//App Component
+class App extends React.Component {
+    
+    //For component Rendering
+    render(){
+
+        //Returns JSX (Javascript syntact extension)
+        return(
+            //Only one element
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Testing</h1> 
+                    </div>
+                </div>       
+            </div>
+        );
+    }
+}
+
+//Render App Component on a element with app ID (<div id="app"></div>)
+render(<App/>, window.document.getElementById("app"));
